dd4ebd358f5afc5a093412bc42a71077212eb37b
Scoutan <johnson.chungck@gmail.com>
2021-05-13 12:05:43 -0400

    .

--- pages/ImageList/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID -_LMbVe13rFXPwAhGb8P2by65hNh3syok9mlKyGdvZQ'
-  }
-})
